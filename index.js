7b1010ecd59ffe8467282bf8014867f4748e47e8
Shel Orock <sdo243@nyu.edu>
2018-03-28 20:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
-var a = Math.floor(Math.random() * 1000)
-var b = Math.floor(Math.random() * 1000)
-
 function add() {
+    
+  var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000)
+
   return a + b
 }
 
